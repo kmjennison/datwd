63d0811253919a0896c708e9933bdb5dc36c1688
Kevin Jennison <kevin.jennison1@gmail.com>
2022-08-11 15:07:40 -0400

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
