6a81ef2f1beb93294c73907b39776614261be7d0
Kevin Jennison <kevin.jennison1@gmail.com>
2022-08-11 15:41:29 -0400

    Add tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,85 @@
+const { execSync } = require('child_process')
+
+jest.mock('child_process', () => ({
+  execSync: jest.fn(),
+}))
+
+const mockNPMList = Buffer.from(
+  JSON.stringify({
+    name: 'example-project',
+    version: '1.0.0',
+    dependencies: {
+      '@some/thing': {
+        version: '7.18.9',
+        dependencies: {
+          foo: {
+            version: '0.98.7',
+          },
+        },
+      },
+      cookies: {
+        version: '0.8.0',
+        dependencies: {
+          depd: {
+            version: '2.0.0',
+          },
+          keygrip: {
+            version: '1.1.0',
+            dependencies: {
+              tsscmp: {
+                version: '1.0.6',
+              },
+            },
+          },
+        },
+      },
+      'another-pkg': {
+        version: '3.3.2',
+        dependencies: {},
+      },
+    },
+  })
+)
+
+beforeEach(() => {
+  execSync.mockReturnValue(mockNPMList)
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
 describe('index.js', () => {
-  it('is a placeholder test', () => {
-    expect(true).toBe(true)
+  it('returns the expected dependencies when there is no filtering', () => {
+    const datwd = require('../index')
+    expect(datwd()).toEqual([
+      '@some/thing',
+      'foo',
+      'cookies',
+      'depd',
+      'keygrip',
+      'tsscmp',
+      'another-pkg',
+    ])
+  })
+
+  it('returns a subset of dependencies when there is filtering', () => {
+    const datwd = require('../index')
+    expect(datwd(['cookies'])).toEqual(['cookies', 'depd', 'keygrip', 'tsscmp'])
+  })
+
+  it('returns one package when filtered on a dependency with no subdependencies', () => {
+    const datwd = require('../index')
+    expect(datwd(['another-pkg'])).toEqual(['another-pkg'])
+  })
+
+  it('returns an empty array when filtering on a *sub*dependency (filters are only for top-level dependencies)', () => {
+    const datwd = require('../index')
+    expect(datwd(['foo'])).toEqual([])
+  })
+
+  it('returns an empty array when filtering on an unused module', () => {
+    const datwd = require('../index')
+    expect(datwd(['this-does-not-exist'])).toEqual([])
   })
 })
